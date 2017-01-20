ff4256ae71afe242a6a23a285b46ee39285803f7
wujiangbo45 <285874318@qq.com>
2017-01-20 10:59:28 +0800

    commit

--- settings.py
+++ settings.py
@@ -92,10 +92,9 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
